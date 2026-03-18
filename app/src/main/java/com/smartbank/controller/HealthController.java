24953b78cad1cd67dbf6c47ac7cb132f7d60a35f
sanjaykumars-tnj <sanjaykumars.tnj@gmail.com>
2026-03-18 21:46:50 +0530

    Code Review changes.
    1. Versioning changes done.
    2. ResponseEntity return for good practice on status code.

--- app/src/main/java/com/smartbank/controller/HealthController.java
+++ app/src/main/java/com/smartbank/controller/HealthController.java
@@ -1,27 +1,32 @@
 package com.smartbank.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
+//versioning should be at the class level
 public class HealthController {
 
-
-    @GetMapping("/v1/health")
-    public Map<String,String> getHeathReport()
+    //converting into responseEntity is good approach to send status code in the response.
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, Object>> getHeathReport()
     {
-        HashMap<String,String> healthInfoMap = new HashMap<>();
+        HashMap<String,Object> healthInfoMap = new HashMap<>();
         healthInfoMap.put("status","UP");
         healthInfoMap.put("service","Smart Bank API");
-
-        return healthInfoMap;
+        healthInfoMap.put("TimeStamp", LocalDateTime.now());
+        return ResponseEntity.ok(healthInfoMap);
 
     }
 
